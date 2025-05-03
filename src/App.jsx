d005e755476dbc55d831595d9809b4b87d0f8b91
Mohammad Ashrafi <ashrafi.dev221184@gmail.com>
2025-05-03 21:37:40 +0330

    Added login and registration form with Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,67 @@
-import React, { Component } from 'react'
 import './App.css'
-
+import React, { Component } from 'react'
 
 export default class App extends Component {
-
-  constructor (props) {
-    super(props)
-
-    this.handlerForm = this.handlerForm.bind(this)
-    this.handlerFname = this.handlerFname.bind(this)
-    this.handlerLname = this.handlerLname.bind(this)
-    this.handlerEmail = this.handlerEmail.bind(this)
-    
-    this.state = {
-      submitted: false,
-      emailData: "",
-      lastNameData: "",
-      firstNameData: "",
-    }
-  }
-
-  
-  
-  handlerForm (event) {
-    event.preventDefault()
-    console.log("Submitted");
-    
-    this.setState({submitted: true})
+  constructor () {
+    super ()
     
   }
-  handlerFname (event) {
-    this.setState({firstNameData: event.target.value})
-
-  }
-  handlerLname (event) {
-    this.setState({lastNameData: event.target.value})
-  }
-  handlerEmail (event) {
-    this.setState({emailData: event.target.value})
-  }
-
   render() {
-    const isFormValid =  this.state.firstNameData && this.state.lastNameData && this.state.emailData;
-
     return (
-      <div className="form-container">
-      <form className="register-form" autoComplete="off" onSubmit={this.handlerForm}>
-
-          {this.state.submitted && isFormValid && (
-            <div className="success-message">Success! Thank you for registering</div>
-          )}
-
-          <input id="first-name" className="form-field" type="text" placeholder="First Name" name="firstName" onChange={this.handlerFname}/>
-          {this.state.firstNameData.length === 0 && this.state.submitted && (
-            <span id="first-name-error">Please enter a first name</span>
-          )}
-
-
-          <input id="last-name" className="form-field" type="text" placeholder="Last Name" name="lastName" onChange={this.handlerLname}/>
-          {this.state.lastNameData.length === 0 && this.state.submitted && (
-            <span id="last-name-error">Please enter a last name</span>
-          )}
-
-          <input id="email" className="form-field" type="text" placeholder="Email" name="email" onChange={this.handlerEmail}/>
-          {this.state.emailData.length === 0 && this.state.submitted && (
-            <span id="email-error">Please enter an email address</span> 
-          )}
-          
-          <button className="form-field" type="submit">
-              Register
-          </button>
-      </form>
-  </div>
+        <div class="section">
+          <div class="container">
+            <div class="row full-height justify-content-center">
+              <div class="col-12 text-center align-self-center py-5">
+                <div class="section pb-5 pt-5 pt-sm-2 text-center">
+                  <h6 class="mb-0 pb-3"><span>Log In </span><span>Sign Up</span></h6>
+                  <input class="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
+                  <label for="reg-log"></label>
+                  <div class="card-3d-wrap mx-auto">
+                    <div class="card-3d-wrapper">
+                      <div class="card-front">
+                        <div class="center-wrap">
+                          <div class="section text-center">
+                            <h4 class="mb-4 pb-3">Log In</h4>
+                            <div class="form-group">
+                              <input type="email" name="logemail" class="form-style" placeholder="Your Email" id="logemail" autocomplete="off" />
+                              <i class="input-icon uil uil-at"></i>
+                            </div>	
+                            <div class="form-group mt-2">
+                              <input type="password" name="logpass" class="form-style" placeholder="Your Password" id="logpass" autocomplete="off" />
+                              <i class="input-icon uil uil-lock-alt"></i>
+                            </div>
+                            <a href="#" class="btn mt-4">submit</a>
+                            <p class="mb-0 mt-4 text-center"><a href="#0" class="link">Forgot your password?</a></p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="card-back">
+                        <div class="center-wrap">
+                          <div class="section text-center">
+                            <h4 class="mb-4 pb-3">Sign Up</h4>
+                            <div class="form-group">
+                              <input type="text" name="logname" class="form-style" placeholder="Your Full Name" id="logname" autocomplete="off" />
+                              <i class="input-icon uil uil-user"></i>
+                            </div>	
+                            <div class="form-group mt-2">
+                              <input type="email" name="logemail" class="form-style" placeholder="Your Email" id="logemail" autocomplete="off" />
+                              <i class="input-icon uil uil-at"></i>
+                            </div>	
+                            <div class="form-group mt-2">
+                              <input type="password" name="logpass" class="form-style" placeholder="Your Password" id="logpass" autocomplete="off" />
+                              <i class="input-icon uil uil-lock-alt"></i>
+                            </div>
+                            <a href="#" class="btn mt-4">submit</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
     )
   }
 }
-
-
